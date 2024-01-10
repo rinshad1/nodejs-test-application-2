1b8db3d5cd2b0fb2d9c74a458c6b4fd2748b51d6
Rinshad V P <vprinshad123@gmail.com>
2024-01-10 13:11:12 +0000

    Updated index.js

--- Application/index.js
+++ Application/index.js
@@ -2,6 +2,6 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Friends.oo...!')
+  res.send('Hello Friends.oo.o..!')
 })
 app.listen(3000, () => console.log('Server is up and running'));
